dcfe585e3985fe6f6b9e1c5aad14973d39d067bf
austinmh12 <45670765+austinmh12@users.noreply.github.com>
2021-03-30 11:35:24 -0400

    Reformatting and added more filtering

--- ebird.py
+++ ebird.py
@@ -11,36 +11,60 @@ def main(coord_file, year):
 	"""Gets the bird data at each hotspot area in the coordinate file for the given year or newer,
 	retrying each missed coordinate once. Saves to a CSV file in the results folder.
 	:param: coord_file (str) - File path of the coordinate list CSV
-	:param: year (int) - Year to filter bird sightings by
+	:param: year (int) - Year to filter bird sightings by, passed to parse_bird_data
 	returns: None
 	"""
 	coord_list = pd.read_csv(coord_file)
-	bird_data_rows = []
-	missed_hotspots_coords = []
-	for y2, x1, y1, x2 in coord_list.values.tolist():
-		try:
-			hotspots = get_hotspots(x1, y1, x2, y2)
-		except Exception:
-			missed_hotspots_coords.append((y2, x1, y1, x2))
-			continue
-		print(f'Total hotspots at {",".join([str(y2),str(x1),str(y1),str(x2)])}: {len(hotspots)}')
-		bird_data = get_birds(hotspots, year)
-		bird_data_rows.extend(bird_data)
-	while missed_hotspots_coords:
-		y2, x1, y1, x2 = missed_hotspots_coords[0]
-		try:
-			hotspots = get_hotspots(x1, y1, x2, y2)
-		except Exception:
-			print(f'Couldn\'t get hotspots for {",".join([str(y2),str(x1),str(y1),str(x2)])}')
-			missed_hotspots_coords.pop()
-			continue
-		print(f'Total hotspots at {",".join([str(y2),str(x1),str(y1),str(x2)])}: {len(hotspots)}')
-		bird_data = get_birds(hotspots)
-		bird_data_rows.extend(bird_data)
-		missed_hotspots_coords.pop()
+	bird_data_rows, missed_hotspots_coords = get_data(coord_list.values.tolist(), year)
+	# for y2, x1, y1, x2 in coord_list.values.tolist():
+	# 	try:
+	# 		hotspots = get_hotspots(x1, y1, x2, y2)
+	# 	except Exception:
+	# 		missed_hotspots_coords.append((y2, x1, y1, x2))
+	# 		continue
+	# 	print(f'Total hotspots at {",".join([str(y2),str(x1),str(y1),str(x2)])}: {len(hotspots)}')
+	# 	bird_data = get_birds(hotspots, year)
+	# 	bird_data_rows.extend(bird_data)
+	if missed_hotspots_coords:
+		missed_bird_data, missed_hotspots_coords_twice = get_data(missed_hotspots_coords, year)
+		bird_data_rows.extend(missed_bird_data)
+		if missed_hotspots_coords_twice:
+			for y2, x1, y1, x2 in missed_hotspots_coords_twice:
+				print(f'Couldn\'t get hotspots for {",".join([str(y2),str(x1),str(y1),str(x2)])}')
+	# while missed_hotspots_coords:
+	# 	y2, x1, y1, x2 = missed_hotspots_coords[0]
+	# 	try:
+	# 		hotspots = get_hotspots(x1, y1, x2, y2)
+	# 	except Exception:
+	# 		print(f'Couldn\'t get hotspots for {",".join([str(y2),str(x1),str(y1),str(x2)])}')
+	# 		missed_hotspots_coords.pop()
+	# 		continue
+	# 	print(f'Total hotspots at {",".join([str(y2),str(x1),str(y1),str(x2)])}: {len(hotspots)}')
+	# 	bird_data = get_birds(hotspots)
+	# 	bird_data_rows.extend(bird_data)
+	# 	missed_hotspots_coords.pop()
+	print(bird_data_rows)
 	df = pd.DataFrame(bird_data_rows, columns=['Hotspot_Name', 'Bird_Name'])
 	df.to_csv(f'results/bird_data-{dt.now().strftime("%Y%m%d%H%M%S")}.csv', index=False)
 
+def get_data(coords, year):
+	"""Function that gets all the hotspots for a given set of coordinates.
+	:param: coords (list) - List of coordinate pairs to get bird data from
+	:param: year (int) - Year to filter bird sightings by, passed to parse_bird_data
+	returns: [bird_data, missed_coordinates]
+	"""
+	data = []
+	missed = []
+	for y2, x1, y1, x2 in coords:
+		try:
+			hotspots = get_hotspots(x1, y1, x2, y2)
+		except Exception:
+			missed.append((y2, x1, y1, x2))
+			continue
+		print(f'Total hotspots at {",".join([str(y2),str(x1),str(y1),str(x2)])}: {len(hotspots)}')
+		bird_data = get_birds(hotspots, year)
+		data.extend(bird_data)
+	return data, missed
 
 def get_hotspots(x1, y1, x2, y2):
 	"""Gets the hotspots from the ebird website
@@ -58,7 +82,7 @@ def get_birds(hotspots, year):
 	"""Retrieves the list of sighted birds from each hotspot passed, filtering by the
 	year passed. Retries each missed hotspot once.
 	:param: hotspots (list(dict)) - The list of hotspots to get the bird data from
-	:param: year (int) - The year to filter sighted birds by
+	:param: year (int) - The year to filter sighted birds by, passed to parse_bird_data
 	returns: list(tuple)
 	"""
 	missed = []
@@ -71,14 +95,17 @@ def get_birds(hotspots, year):
 			missed.append(hotspot)
 			sleep(.5)
 			continue
-		lis = soup.find_all(id=re.compile('has-det-\d*'))
-		for li in lis:
-			bird_name_div = li.find_next('div').find_next('div')
-			bird_name = bird_name_div.span.string
-			info_div = bird_name_div.find_next('div')
-			date = list(info_div.stripped_strings)[2]
-			if int(date[-4:]) >= 2010:
-				ret.append((hotspot['n'], bird_name))
+		birds = parse_bird_data(soup, year)
+		if birds:
+			ret.extend([(hotspot['n'], bird) for bird in birds])
+		# lis = soup.find_all(id=re.compile('has-det-\d*'))
+		# for li in lis:
+		# 	bird_name_div = li.find_next('div').find_next('div')
+		# 	bird_name = bird_name_div.span.string
+		# 	info_div = bird_name_div.find_next('div')
+		# 	date = list(info_div.stripped_strings)[2]
+		# 	if int(date[-4:]) >= 2010:
+		# 		ret.append((hotspot['n'], bird_name))
 		sleep(.5)
 	while missed:
 		try:
@@ -89,16 +116,41 @@ def get_birds(hotspots, year):
 			missed.pop()
 			sleep(.5)
 			continue
-		lis = soup.find_all(id=re.compile('has-det-\d*'))
-		for li in lis:
-			bird_name_div = li.find_next('div').find_next('div')
-			bird_name = bird_name_div.span.string
-			info_div = bird_name_div.find_next('div')
-			date = list(info_div.stripped_strings)[2]
-			if int(date[-4:]) >= 2010:
-				ret.append((missed[0]['n'], bird_name))
+		birds = parse_bird_data(soup, year)
+		if birds:
+			ret.extend([(hotspot['n'], bird) for bird in birds])
+		# lis = soup.find_all(id=re.compile('has-det-\d*'))
+		# for li in lis:
+		# 	bird_name_div = li.find_next('div').find_next('div')
+		# 	bird_name = bird_name_div.span.string
+		# 	info_div = bird_name_div.find_next('div')
+		# 	date = list(info_div.stripped_strings)[2]
+		# 	if int(date[-4:]) >= 2010:
+		# 		ret.append((missed[0]['n'], bird_name))
 		missed.pop()
 		sleep(.5)
+	return ret
+
+def parse_bird_data(html, year):
+	"""Parses the html from eBird for the hotspot into a list of bird names,
+	filtering out hybrids and unspecific bird species, as well as sighting year.
+	:param: html (BeautifulSoup) - HTML of the hotspot page
+	:param: year (int) - Year to filter sightings by.
+	returns: list(str)
+	"""
+	ret = []
+	lis = html.find_all(id=re.compile('has-det-\d*'))
+	for li in lis:
+		bird_name_div = li.find_next('div').find_next('div')
+		bird_name = bird_name_div.span.string
+		if re.match(r'.* x .*', bird_name): # Filtering hybrids (bird x bird)
+			continue
+		if re.match(r'.*(/|\(|sp\.).*', bird_name): # Filtering Bird/Bird, Bird sp., and Bird (hybrid)
+			continue
+		info_div = bird_name_div.find_next('div')
+		date = list(info_div.stripped_strings)[2]
+		if int(date[-4:]) >= year:
+			ret.append(bird_name)
 	return ret
 
 CMD = ArgumentParser()
